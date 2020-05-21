2d0f2eb151f8cf42c70662a3da39a7b4ef170a15
Anlifa <46711815+Anlifa@users.noreply.github.com>
2020-05-21 21:50:42 +0300

    Add files via upload

---
+++ terraform/variable.tf
@@ -0,0 +1,17 @@
+variable "credentials" {
+    default = "anna.json"
+}
+variable "project" {
+  default = "watchful-slice-274910"
+  description = "Project id"
+}
+
+variable "region" {
+  default = "europe-north1"
+  description = "region"
+}
+
+variable "zone" {
+  default = "europe-north1-a"
+  description = "zone"
+}
